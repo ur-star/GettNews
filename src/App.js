1de10ad44b662a3413f26c9d81114995b3f7a4f4
ur-star <61001703+ur-star@users.noreply.github.com>
2022-06-04 18:15:22 +0530

    code formatted and comment added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Newsbody from './components/Newsbody';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Newsbody from "./components/Newsbody";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      {/* <h1>Hellowald</h1> */}
-      <Navbar/>
-      <Switch>
-          <Route exact  path="/"><Newsbody key={'general'} pagesize={12} country="in" category="general"/></Route>
-          <Route exact path="/science"><Newsbody key={'science'} pagesize={12} country="in" category="science"/></Route>
-          <Route exact path="/business"><Newsbody key={'business'} pagesize={12} country="in" category="business"/></Route>
-          <Route exact path="/technology"><Newsbody key={'technology'} pagesize={12} country="in" category="technology"/></Route>
-          <Route exact path="/health"><Newsbody key={'health'} pagesize={12} country="in" category="health"/></Route>
-          <Route exact path="/sports"><Newsbody key={'sports'} pagesize={12} country="in" category="sports"/></Route>
-          
+      <Router>
+        <Navbar />
+        {/* key is added so every time we change route it refetches the news otherwise we need to reload */}
+        <Switch>
+          <Route exact path="/">
+            <Newsbody
+              key={"general"}
+              pagesize={12}
+              country="in"
+              category="general"
+            />
+          </Route>
+          <Route exact path="/science">
+            <Newsbody
+              key={"science"}
+              pagesize={12}
+              country="in"
+              category="science"
+            />
+          </Route>
+          <Route exact path="/business">
+            <Newsbody
+              key={"business"}
+              pagesize={12}
+              country="in"
+              category="business"
+            />
+          </Route>
+          <Route exact path="/technology">
+            <Newsbody
+              key={"technology"}
+              pagesize={12}
+              country="in"
+              category="technology"
+            />
+          </Route>
+          <Route exact path="/health">
+            <Newsbody
+              key={"health"}
+              pagesize={12}
+              country="in"
+              category="health"
+            />
+          </Route>
+          <Route exact path="/sports">
+            <Newsbody
+              key={"sports"}
+              pagesize={12}
+              country="in"
+              category="sports"
+            />
+          </Route>
         </Switch>
-      
       </Router>
     </div>
   );
--- src/components/Newsbody.jsx
+++ src/components/Newsbody.jsx
@@ -1,72 +1,83 @@
 import React, { Component } from "react";
 import NewsItems from "./NewsItems";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 export class Newsbody extends Component {
   static defaultProps = {
-      country: "in",
-      pagesize: 5,
-      category: "general",
-    }
-    static propTypes = {
-        country: PropTypes.string,
-        pagesize: PropTypes.number,
-        category: PropTypes.string,
-    }
+    country: "in",
+    pagesize: 5,
+    category: "general",
+  };
+  static propTypes = {
+    country: PropTypes.string,
+    pagesize: PropTypes.number,
+    category: PropTypes.string,
+  };
   constructor() {
-   
     super();
     this.state = {
       articles: [],
       // totalResults:0,
-      loading:false,
+      loading: false,
       page: 1,
     };
   }
-  async componentDidMount(){
-    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=da56685e2150488398fb5947b1ecc420&page=1&pageSize=${this.props.pagesize}`;
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=da56685e2150488398fb5947b1ecc420&page=1&pageSize=${this.props.pagesize}`;
     let data = await fetch(url);
     let jsdata = await data.json();
     console.log(jsdata);
     // console.log(this.props.pagesize);
-    
-  this.setState({articles: jsdata.articles, totalResults:jsdata.totalResults})
-  // console.log(jsdata.totalResults);
-  // console.log(this.state.totalResults);
-}
 
-   handlepreClick = async()=>{
-    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=da56685e2150488398fb5947b1ecc420&page=${this.state.page - 1}&pageSize=${this.props.pagesize}`;
+    this.setState({
+      articles: jsdata.articles,
+      totalResults: jsdata.totalResults,
+    });
+    // console.log(jsdata.totalResults);
+    // console.log(this.state.totalResults);
+  }
+
+  handlepreClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${
+      this.props.category
+    }&apiKey=da56685e2150488398fb5947b1ecc420&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pagesize}`;
     let data = await fetch(url);
     let jsdata = await data.json();
- 
-  this.setState({
-    page: this.state.page-1,
-    articles: jsdata.articles
-  })
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: jsdata.articles,
+    });
   };
-  
-  handlenxtClick = async()=>{
-    if(Math.ceil(this.state.totalresults/this.props.pagesize) < this.state.page+1){
+
+  handlenxtClick = async () => {
+    if (
+      Math.ceil(this.state.totalresults / this.props.pagesize) <
+      this.state.page + 1
+    ) {
       // console.log('inside if nextclick');
-    }
-    else{
+    } else {
       // console.log('inside else nxtclick');
-      let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=da56685e2150488398fb5947b1ecc420&page=${this.state.page + 1}&pageSize=${this.props.pagesize}`;
+      let url = `https://newsapi.org/v2/top-headlines?country=${
+        this.props.country
+      }&category=${
+        this.props.category
+      }&apiKey=da56685e2150488398fb5947b1ecc420&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pagesize}`;
       let data = await fetch(url);
       let jsdata = await data.json();
-   
-    this.setState({
-      page: this.state.page+1,
-      articles: jsdata.articles
-    })
-  }
-      
- };
 
- 
-   
-  
+      this.setState({
+        page: this.state.page + 1,
+        articles: jsdata.articles,
+      });
+    }
+  };
 
   render() {
     return (
@@ -79,8 +90,10 @@ export class Newsbody extends Component {
             return (
               <div className="col-md-4" key={ele.url}>
                 <NewsItems
-                  imageUrl={ele.urlToImage?ele.urlToImage:require('../error404.jpg')}
-                  title={ele.title?ele.title:"There is an intersting News"}
+                  imageUrl={
+                    ele.urlToImage ? ele.urlToImage : require("../error404.jpg")
+                  }
+                  title={ele.title ? ele.title : "There is an intersting News"}
                   url={ele.url}
                 />
               </div>
@@ -88,12 +101,27 @@ export class Newsbody extends Component {
           })}
         </div>
         <div className="container d-flex justify-content-between">
-        <button type="button" disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlepreClick}>&larr; Previous</button>
-        <button type="button" disabled={Math.ceil(this.state.totalresults/this.props.pagesize) > this.state.page+1} className="btn btn-dark" onClick={this.handlenxtClick}>Next &rarr;</button>
-
+          <button
+            type="button"
+            disabled={this.state.page <= 1}
+            className="btn btn-dark"
+            onClick={this.handlepreClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            disabled={
+              Math.ceil(this.state.totalresults / this.props.pagesize) >
+              this.state.page + 1
+            }
+            className="btn btn-dark"
+            onClick={this.handlenxtClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
-     
     );
   }
 }
