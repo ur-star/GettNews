286e4c356011221f2b24a2620245b2dbbbf0428b
ur-star <61001703+ur-star@users.noreply.github.com>
2022-06-08 21:58:54 +0530

    infinte scroll added using react-infinite scroll

--- src/components/Newsbody.jsx
+++ src/components/Newsbody.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import NewsItems from "./NewsItems";
 import PropTypes from "prop-types";
 // require('dotenv').config();
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 
 export class Newsbody extends Component {
   static defaultProps = {
@@ -33,20 +35,10 @@ export class Newsbody extends Component {
       articles: jsdata.articles,
       totalResults: jsdata.totalResults,
     });
+    // console.log(this.state.articles.length);
   }
   async componentDidMount() {
-    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=da56685e2150488398fb5947b1ecc420&page=1&pageSize=${this.props.pagesize}`;
-    // let data = await fetch(url);
-    // let jsdata = await data.json();
-    // console.log(jsdata);
-    // // console.log(this.props.pagesize);
-
-    // this.setState({
-    //   articles: jsdata.articles,
-    //   totalResults: jsdata.totalResults,
-    // });
-    // // console.log(jsdata.totalResults);
-    // // console.log(this.state.totalResults);
+   
     this.updateNews();
   }
 
@@ -67,6 +59,7 @@ export class Newsbody extends Component {
     // });
     this.setState({ page: this.state.page - 1 });
     this.updateNews();
+    
   };
 
   handlenxtClick = async () => {
@@ -95,6 +88,18 @@ export class Newsbody extends Component {
     this.setState({ page: this.state.page + 1 });
     this.updateNews();
   };
+   fetchMoreData = async () => {
+  this.setState({ page: this.state.page + 1 });
+  let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=da56685e2150488398fb5947b1ecc420&page=${this.state.page}&pageSize=${this.props.pagesize}`;
+    let data = await fetch(url);
+    let jsdata = await data.json();
+
+    this.setState({
+      articles: this.state.articles.concat(jsdata.articles),
+      totalResults: jsdata.totalResults,
+    });
+  
+  }
 
   render() {
     return (
@@ -102,7 +107,15 @@ export class Newsbody extends Component {
         <h2>Top {this.props.category} Headlines</h2>
 
         <hr />
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+        <div className="container">
         <div className="row my-4">
+        
           {this.state.articles.map((ele) => {
             return (
               <div className="col-md-4" key={ele.url}>
@@ -124,8 +137,11 @@ export class Newsbody extends Component {
               </div>
             );
           })}
+          
         </div>
-        <div className="container d-flex justify-content-between">
+        </div>
+        </InfiniteScroll>
+        {/* <div className="container d-flex justify-content-between">
           <button
             type="button"
             disabled={this.state.page <= 1}
@@ -145,7 +161,7 @@ export class Newsbody extends Component {
           >
             Next &rarr;
           </button>
-        </div>
+        </div> */}
       </div>
     );
   }
